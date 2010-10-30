db1b5ed9045f82861e3b4e1abee2da7acd1f2094
Hatem Mahmoud <hatem@expressionlab.com>
2010-10-30 10:16:48 +0200

    revert devise integration

--- Gemfile
+++ Gemfile
@@ -1,7 +1,6 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.1'
-gem 'devise'
 
 group :development do
   gem 'sqlite3-ruby', :require => 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Storemate::Application.routes.draw do
-  devise_for :users
-
   resources :pages, :only => :show
   root :to => "pages#show", :id => :home
 
