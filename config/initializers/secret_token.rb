fe504d4780860ef375b858f32796179ff0c98a6a
Hatem Mahmoud <hatem@expressionlab.com>
2011-09-23 19:05:44 +0200

    rails new storemate --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Storemate::Application.config.secret_token = 'dcbaee2d88f7599ed3c342a0f2f90407f5538f52a87d6cacfdde21e4944fed503572b66ab9f40219c6e46e9af225671a4fee3399da0f4bb79f010efc53ab82a3'
